4123d7f70b3f64b10d1ea0e512e8254cad438851
Sophie <smf115@ic.ac.uk>
2020-12-18 20:22:45 +0000

    Small change to program path order

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,18 +31,16 @@ def pytest_addoption(parser):
     parser.addoption('--gulp_path', default='')
 
 def pytest_generate_tests(metafunc):
-    if 'macromodel_path' in metafunc.fixturenames:
-        mm_path = metafunc.config.getoption('macromodel_path')
-        metafunc.parametrize('macromodel_path', [mm_path])
-    if 'mopac_path' in metafunc.fixturenames:
-        mopac_path = metafunc.config.getoption('mopac_path')
-        metafunc.parametrize('mopac_path', [mopac_path])
-    if 'xtb_path' in metafunc.fixturenames:
-        xtb_path = metafunc.config.getoption('xtb_path')
-        metafunc.parametrize('xtb_path', [xtb_path])
     if 'gulp_path' in metafunc.fixturenames:
         gulp_path = metafunc.config.getoption('gulp_path')
         metafunc.parametrize('gulp_path', [gulp_path])
-    # elif 'macromodel_path' in metafunc.fixturenames:
-    #     macromodel_path = metafunc.config.getoption('macromodel_path')
-    #     metafunc.parametrize('macromodel_path', [macromodel_path])
+    elif 'macromodel_path' in metafunc.fixturenames:
+        mm_path = metafunc.config.getoption('macromodel_path')
+        metafunc.parametrize('macromodel_path', [mm_path])
+    elif 'mopac_path' in metafunc.fixturenames:
+        mopac_path = metafunc.config.getoption('mopac_path')
+        metafunc.parametrize('mopac_path', [mopac_path])
+    elif 'xtb_path' in metafunc.fixturenames:
+        xtb_path = metafunc.config.getoption('xtb_path')
+        metafunc.parametrize('xtb_path', [xtb_path])
+    
